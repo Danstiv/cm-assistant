5e853c942b1ca7f3dd55ec6aa3bfe24b51e86c6f
Danstiv <50794055+Danstiv@users.noreply.github.com>
2022-06-14 03:01:02 +0700

    Refactor message handling

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -7,8 +7,6 @@ import sys
 
 import dotenv
 import pyrogram
-from pyrogram.enums.parse_mode import ParseMode
-from pyrogram.utils import get_peer_type
 try:
     import uvloop
 except ImportError:
@@ -17,12 +15,10 @@ except ImportError:
 from . import custom_methods
 from . import db
 from . import exception_handler
-from . import split_text
-from .keyboard_handler import KeyboardHandler
-from .limiter import Limiter
-from .logging_helpers import Formatter, WarningErrorHandler
 from .handler_decorators import get_handlers
-from .prioritized_item import PrioritizedItem
+from .keyboard_handler import KeyboardHandler
+from .logging_helpers import Formatter, WarningErrorHandler
+from .message_handler import MessageHandler
 
 dotenv.load_dotenv()
 
@@ -31,9 +27,11 @@ LOG_MAX_BACKUPS = 9
 
 
 class BotController(
+    MessageHandler,
     KeyboardHandler,
     db.DB,
 ):
+
     def __init__(self, bot_name, api_id=None, api_hash=None, use_uvloop=False):
         self.api_id = api_id or os.environ['API_ID']
         self.api_hash = api_hash or os.environ['API_HASH']
@@ -73,16 +71,12 @@ class BotController(
         warning_error_handler.setFormatter(detailed_formatter)
         self.log.addHandler(warning_error_handler)
         self.app = None
-        self.message_queue = asyncio.PriorityQueue()
-        self.message_id = 1
-        self.global_message_limiter = Limiter(self, 30, 1, name='broadcast_messages')
-        self.message_limiters = {}
-        self.message_event_chains = {}
         if sys.platform != 'win32' and use_uvloop:
             if not uvloop:
                 self.log.warning('uvloop не установлен')
             else:
                 uvloop.install()
+        super().__init__()
 
     def add_task(self, callable, *args, name=None, **kwargs):
         name = name or callable.__name__
@@ -123,9 +117,6 @@ class BotController(
     def get_global_filter(self):
         pass
 
-    def get_default_chat_id(self):
-        pass
-
     async def initialize(self):
         self.app.controller = self
         pyrogram.types.Message.reply = custom_methods.reply
@@ -157,7 +148,7 @@ class BotController(
         )
         await self.initialize()
         await self.app.start()
-        self.add_task(self.message_sender, 42)
+        self.add_task(self.message_sender, 23)
         self.log.info('Приложение запущено')
         try:
             await self.monitor_tasks()
@@ -174,127 +165,3 @@ class BotController(
         self.canceling = True
         self.monitor_task.cancel()
         [task.cancel() for task in self.async_tasks]
-
-    def get_message_texts(self, text, title='', **kwargs):
-        return split_text.split_text_by_units(header=title, body=text, max_part_length=4096, **kwargs)
-
-    def send_message_sync(self, text, *args, priority=2, blocking=False, **kwargs):
-        args = list(args)
-        texts = self.get_message_texts(text, title=kwargs.get('title', ''))
-        self.log.debug(f'Постановка {len(texts)} частей сообщения в очередь с приоритетом {priority} ({"блокирующая" if blocking else "неблокирующая"} отправка, текущий id {self.message_id})')
-        if not blocking:
-            for text in texts:
-                message_data = {
-                    'args': [text]+args,
-                    'kwargs': kwargs,
-                }
-                self.message_queue.put_nowait(PrioritizedItem(priority, self.message_id, message_data))
-                self.message_id += 1
-            self.log.debug(f'Неблокирующие сообщения поставлены в очередь (текущий id {self.message_id})')
-            return
-        semaphore = asyncio.Semaphore(len(texts))
-        event = asyncio.Event()
-        for text in texts:
-            message_data = {
-                'args': [text]+args,
-                'kwargs': kwargs,
-                'semaphore': semaphore,
-                'event': event,
-            }
-            self.message_queue.put_nowait(PrioritizedItem(priority, self.message_id, message_data))
-            self.message_id += 1
-        self.log.debug(f'Блокирующие сообщения поставлены в очередь (текущий id {self.message_id})')
-        return event
-
-    async def send_message(self, *args, **kwargs):
-        event = self.send_message_sync(*args, **kwargs)
-        if event:
-            await event.wait()
-            return event.message
-
-    def send_warning_error_message_sync(self, *args, **kwargs):
-        self.send_message_sync(
-            *args,
-            priority=1,
-            ignore_errors=True,
-            parse_mode=ParseMode.DISABLED,
-            **kwargs,
-        )
-
-    async def message_sender(self, max_concurrent_sendings):
-        message_tasks = []
-        get_message_task = None
-        while True:
-            if not get_message_task and len(message_tasks) < max_concurrent_sendings:
-                get_message_task = asyncio.create_task(self.message_queue.get())
-                message_tasks.append(get_message_task)
-            try:
-                done_tasks, pending_tasks = await asyncio.wait(message_tasks, return_when=asyncio.FIRST_COMPLETED)
-            except asyncio.CancelledError:
-                [t.cancel() for t in message_tasks]
-                raise
-            for task in done_tasks:
-                message_tasks.remove(task)
-                if task == get_message_task:
-                    get_message_task = None
-                    item = task.result()
-                    message_id = item.id
-                    priority = item.priority
-                    message_data = item.item
-                    while True:
-                        try:
-                            self.log.debug(f'Создаётся задача для отправки сообщения с приоритетом {priority}, id {message_id}')
-                            message_task = asyncio.create_task(self.send_message_blocking(*message_data['args'], priority=priority, **message_data['kwargs']))
-                        except Exception:
-                            ignore_errors = message_data.get('kwargs', {}).get('ignore_errors', False)
-                            (self.log.info if ignore_errors else self.log.error)('Необработанное исключение при создании задачи для отправки сообщения:', exc_info=True)
-                        else:
-                            message_task.message_data = message_data
-                            message_tasks.append(message_task)
-                        if len(message_tasks) >= max_concurrent_sendings or self.message_queue.empty():
-                            break
-                        item = self.message_queue.get_nowait()
-                        message_id = item.id
-                        priority = item.priority
-                        message_data = item.item
-                    continue
-                result = None
-                try:
-                    event = task.message_data.get('event')
-                    semaphore = task.message_data.get('semaphore')
-                    result = task.result()
-                except Exception:
-                    ignore_errors = message_data['kwargs'].get('ignore_errors', False)
-                    (self.log.info if ignore_errors else self.log.error)('Необработанное исключение при отправке сообщения:', exc_info=True)
-                finally:
-                    if semaphore:
-                        await semaphore.acquire()
-                        if semaphore.locked():
-                            event.message = result
-                            event.set()
-
-    async def send_message_blocking(self, text, /, chat_id=None, ignore_errors=False, priority=None, **kwargs):
-        chat_id = chat_id or self.get_default_chat_id()
-        if chat_id not in self.message_limiters:
-            if get_peer_type(chat_id) == 'user':
-                limiter = Limiter(self, 3, 1, name=f'user_{chat_id}', static=False)
-            else:
-                limiter = Limiter(self, 20, 60, name=f'chat_{chat_id}', static=False)
-            self.message_limiters[chat_id] = limiter
-        else:
-            limiter = self.message_limiters[chat_id]
-        event_chain = {}
-        if priority is not None:
-            key = (chat_id, priority)
-            if key not in self.message_event_chains:
-                event_chain = {'previous_invoke_event': None, 'current_invoke_event': asyncio.Event()}
-            else:
-                event_chain = self.message_event_chains[key]
-            new_chain = {'previous_invoke_event': event_chain['current_invoke_event'], 'current_invoke_event': asyncio.Event()}
-            self.message_event_chains[key] = new_chain
-        self.log.debug(f'Отправка сообщения в чат {chat_id}')
-        try:
-            return await self.app.send_message(chat_id, text, ignore_errors=ignore_errors, limiters=[self.global_message_limiter, limiter], **kwargs|event_chain)
-        except Exception:  # Probably exceptions shouldn't freeze sending messages
-            event_chain['current_invoke_event'].set()
-            raise
---
+++ tgbot/message_handler.py
@@ -0,0 +1,165 @@
+import asyncio
+from collections import OrderedDict
+
+from pyrogram.enums.parse_mode import ParseMode
+from pyrogram.utils import get_peer_type
+
+from . import split_text
+from .limiter import Limiter
+from .prioritized_item import PrioritizedItem
+
+
+class MessageHandler:
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.message_queue = asyncio.PriorityQueue()
+        self.message_id = 1
+        self.global_message_limiter = Limiter(
+            self,
+            30,
+            1,
+            name='broadcast_messages'
+        )
+        self.message_limiters = {}
+        self.message_event_chains = {}
+        self.messages_info = OrderedDict()
+        for priority in range(1, 4):
+            self.messages_info[priority] = {'pending': 0, 'processing': 0}
+
+    def get_default_chat_id(self):
+        pass
+
+    def get_message_texts(self, text, title='', **kwargs):
+        return split_text.split_text_by_units(header=title, body=text, max_part_length=4096, **kwargs)
+
+    def send_message_sync(self, text, /, chat_id=None, *args, priority=2, blocking=False, **kwargs):
+        chat_id = chat_id or self.get_default_chat_id()
+        if chat_id not in self.message_limiters:
+            if get_peer_type(chat_id) == 'user':
+                limiter = Limiter(self, 3, 1, name=f'user_{chat_id}', static=False)
+            else:
+                limiter = Limiter(self, 20, 60, name=f'chat_{chat_id}', static=False)
+            self.message_limiters[chat_id] = limiter
+        else:
+            limiter = self.message_limiters[chat_id]
+        kwargs['limiters'] = [self.global_message_limiter, limiter]
+        event_chain_key = (chat_id, priority)
+        if event_chain_key not in self.message_event_chains:
+            event_chain = {}
+        else:
+            event_chain = self.message_event_chains[event_chain_key]
+        info = self.messages_info[priority]
+        texts = self.get_message_texts(text, title=kwargs.get('title', ''))
+        self.log.debug(f'Постановка {len(texts)} частей сообщения в очередь с приоритетом {priority} ({"блокирующая" if blocking else "неблокирующая"} отправка, текущий id {self.message_id})')
+        for i, text in enumerate(texts):
+            event_chain['current_invoke_event'] = asyncio.Event()
+            new_event_chain = {'previous_invoke_event': event_chain['current_invoke_event']}
+            self.message_event_chains[event_chain_key] = new_event_chain
+            send_message_coroutine = self.app.send_message(
+                chat_id,
+                text,
+                *args,
+                **kwargs|event_chain
+            )
+            finish_event = None
+            if blocking and i == len(texts)-1:
+                finish_event = asyncio.Event()
+            message_data = {
+                'coroutine': send_message_coroutine,
+                'priority': priority,
+                'message_id': self.message_id,
+                'chat_id': chat_id,
+                'ignore_errors': kwargs.get('ignore_errors', False),
+                'finish_event': finish_event,
+                'current_invoke_event': event_chain['current_invoke_event'],
+            }
+            event_chain = new_event_chain
+            self.message_queue.put_nowait(PrioritizedItem(priority, self.message_id, message_data))
+            info['pending'] += 1
+            self.message_id += 1
+        self.log.debug(f'Сообщения поставлены в очередь (текущий id {self.message_id})')
+        if blocking:
+            return finish_event
+
+    async def send_message(self, *args, **kwargs):
+        event = self.send_message_sync(*args, **kwargs)
+        if event:
+            await event.wait()
+            return event.message
+
+    def send_warning_error_message_sync(self, *args, **kwargs):
+        self.send_message_sync(
+            *args,
+            priority=1,
+            ignore_errors=True,
+            parse_mode=ParseMode.DISABLED,
+            **kwargs,
+        )
+
+    async def message_sender(self, max_concurrent_sendings_per_priority):
+        def could_get_next_item():
+            next_item_priority = None
+            next_item_priority_queue_size = None
+            first_full_queue_priority = None
+            for priority, info in self.messages_info.items():
+                if next_item_priority is None and info['pending'] > 0:
+                    next_item_priority = priority
+                    next_item_priority_queue_size = info['processing']
+                if first_full_queue_priority is None and info['processing'] == max_concurrent_sendings_per_priority:
+                    first_full_queue_priority = priority
+                if next_item_priority is not None and first_full_queue_priority is not None:
+                    break
+            if first_full_queue_priority is None:
+                return True
+            if next_item_priority is None:
+                return False
+            if next_item_priority_queue_size >= max_concurrent_sendings_per_priority:
+                return False
+            return True
+        message_tasks = []
+        get_message_task = None
+        while True:
+            if not get_message_task and could_get_next_item():
+                get_message_task = asyncio.create_task(self.message_queue.get())
+                message_tasks.append(get_message_task)
+            try:
+                done_tasks, pending_tasks = await asyncio.wait(message_tasks, return_when=asyncio.FIRST_COMPLETED)
+            except asyncio.CancelledError:
+                [t.cancel() for t in message_tasks]
+                raise
+            for task in done_tasks:
+                message_tasks.remove(task)
+                if task == get_message_task:
+                    get_message_task = None
+                    item = task.result()
+                    while True:
+                        priority = item.priority
+                        message_id = item.id
+                        message_data = item.item
+                        self.log.debug(f'Создаётся задача для отправки сообщения в чат {message_data["chat_id"]} с приоритетом {priority}, id {message_id}')
+                        message_task = asyncio.create_task(message_data['coroutine'])
+                        message_task.message_data = message_data
+                        message_tasks.append(message_task)
+                        info = self.messages_info[priority]
+                        info['pending'] -= 1
+                        info['processing'] += 1
+                        if self.message_queue.empty() or not could_get_next_item():
+                            break
+                        item = self.message_queue.get_nowait()
+                    continue
+                result = None
+                try:
+                    message_data = task.message_data
+                    result = task.result()
+                except Exception:
+                    message_data['current_invoke_event'].set()
+                    (self.log.info if message_data['ignore_errors'] else self.log.error)(
+                        f'Необработанное исключение при отправке сообщения {message_data["message_id"]}:',
+                        exc_info=True
+                    )
+                finally:
+                    self.messages_info[message_data['priority']]['processing'] -= 1
+                    if message_data['finish_event']:
+                        message_data['finish_event'].message = result
+                        message_data['finish_event'].set()
