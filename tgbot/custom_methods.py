c2a430cfebf24dcb1696dbcab5172ee42e5bc2a2
Danstiv <50794055+Danstiv@users.noreply.github.com>
2022-06-15 01:10:30 +0700

    Fix custom reply method

--- tgbot/custom_methods.py
+++ tgbot/custom_methods.py
@@ -1,3 +1,4 @@
 async def reply(self, *args, **kwargs):
+    kwargs['chat_id'] = self.chat.id
     kwargs['reply_to_message_id'] = self.id
     return await self._client.controller.send_message(*args, **kwargs)
