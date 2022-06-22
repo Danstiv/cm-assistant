4a04a002985ecea55a1bcb1ac73fc41ec4176ac7
Danstiv <50794055+Danstiv@users.noreply.github.com>
2022-06-23 00:35:37 +0700

    Implement bot token loading from .env

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -33,7 +33,7 @@ class BotController(
 ):
 
     def __init__(self, bot_name, use_uvloop=False, user_table=None):
-        for var in ['api_id', 'api_hash', 'db_url', 'dev_ids']:
+        for var in ['api_id', 'api_hash', 'bot_token', 'db_url', 'dev_ids']:
             setattr(self, var, os.getenv(var.upper()))
             if not getattr(self, var):
                 raise RuntimeError(f'"{var.upper()}" environment variable not specified')
@@ -77,6 +77,7 @@ class BotController(
             'telegram_account',
             api_id=self.api_id,
             api_hash=self.api_hash,
+            bot_token=self.bot_token,
             workdir='.',
             sleep_threshold=0,
         )
