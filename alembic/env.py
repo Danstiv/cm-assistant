4d05ea5041e332df95590466ee25a539bacd3658
Danstiv <50794055+Danstiv@users.noreply.github.com>
2022-09-03 17:21:50 +0700

    Remove states

--- alembic/env.py
+++ alembic/env.py
@@ -18,7 +18,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from tables import Base, states
+from tables import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
--- tgbot/handler_decorators.py
+++ tgbot/handler_decorators.py
@@ -2,7 +2,6 @@ from functools import wraps
 import sys
 
 import pyrogram
-from tgbot.states import current_state
 
 handlers = []
 
@@ -16,22 +15,11 @@ def clear_handlers():
 
 
 def make_handler_decorator(decorator_name):
-    def handler_decorator(*handler_args, state=None, **handler_kwargs):
+    def handler_decorator(*handler_args, **handler_kwargs):
         def decorator(func):
             handler_name = func.__name__
             @wraps(func)
             async def wrapper(self, client, *args):
-                nonlocal state
-                if state is not None:
-                    if isinstance(args[0], pyrogram.types.Message):
-                        message = args[0]
-                    else:
-                        self.log.error(f'Обработчик {handler_name}. Возможность использования состояний не реализована для этого типа обработчиков.')
-                        return
-                    state_obj = await state[0].get_state(self, state[1], message.chat, message.from_user)
-                    if not state_obj:
-                        raise pyrogram.ContinuePropagation
-                    current_state.set_context_var_value(state_obj)
                 try:
                     return await func(self, *args)
                 except (pyrogram.ContinuePropagation, pyrogram.StopPropagation):
